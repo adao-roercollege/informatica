cbda3ed4610172b2b0d57c5de958a22228783093
adao-roercollege <soml106925@soml.nl>
2025-01-08 15:15:30 +0100

    icons

--- script.js
+++ script.js
@@ -1,28 +1,50 @@
-  function generateRandomText() {
-    const words = ["Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit", "Sed", "do", "eiusmod", "tempor", "incididunt", "ut", "labore", "et", "dolore", "magna", "aliqua"];
-    let text = '';
-    const wordCount = Math.floor(Math.random() * 10) + 5;
-    for (let i = 0; i < wordCount; i++) {
-      text += `${words[Math.floor(Math.random() * words.length)]} `;
-    }
-    return `${text.trim()}.`;
-  }
+function generateRandomText() {
+	const words = [
+		"Lorem",
+		"ipsum",
+		"dolor",
+		"sit",
+		"amet",
+		"consectetur",
+		"adipiscing",
+		"elit",
+		"Sed",
+		"do",
+		"eiusmod",
+		"tempor",
+		"incididunt",
+		"ut",
+		"labore",
+		"et",
+		"dolore",
+		"magna",
+		"aliqua",
+	];
+	let text = "";
+	const wordCount = Math.floor(Math.random() * 10) + 5;
+	for (let i = 0; i < wordCount; i++) {
+		text += `${words[Math.floor(Math.random() * words.length)]} `;
+	}
+	return `${text.trim()}.`;
+}
 
-document.addEventListener('DOMContentLoaded', () => {
-  const projectContainer = document.querySelector('.project-grid');
-  const NUM_PROJECTS = 8;
-  projectContainer.innerHTML = '';
+document.addEventListener("DOMContentLoaded", () => {
+	lucide.createIcons();
 
-  for(let i = 1; i <= NUM_PROJECTS; i++) {
-    const projectCard = document.createElement('div');
-    projectCard.className = 'project-card';
-    
-    projectCard.innerHTML = `
+	const projectContainer = document.querySelector(".project-grid");
+	const NUM_PROJECTS = 12;
+	projectContainer.innerHTML = "";
+
+	for (let i = 1; i <= NUM_PROJECTS; i++) {
+		const projectCard = document.createElement("div");
+		projectCard.className = "project-card";
+
+		projectCard.innerHTML = `
       <img src="https://picsum.photos/id/${Math.floor(Math.random() * 1085)}/400/200?grayscale&blur=2" alt="Project ${i} Image">
       <h3>Project ${i}</h3>
       <p>${generateRandomText()}</p>
     `;
-    
-    projectContainer.appendChild(projectCard);
-  }
+
+		projectContainer.appendChild(projectCard);
+	}
 });
