564d4bb3b5b2556f0a7e53c866f5a45b42fd2390
adao-roercollege <soml106925@soml.nl>
2025-02-11 16:00:24 +0100

    oops

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function generateRandomText(wordCount) {
+function generate_random_text(word_count) {
 	const words = [
 		"Lorem",
 		"ipsum",
@@ -21,30 +21,30 @@ function generateRandomText(wordCount) {
 		"aliqua",
 	];
 	let text = "";
-	for (let i = 0; i < wordCount; i++) {
+	for (let i = 0; i < word_count; i++) {
 		text += `${words[Math.floor(Math.random() * words.length)]} `;
 	}
 	return `${text.trim()}.`;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+	const num_projects = 12;
+	const project_description_words = 15;
+
 	lucide.createIcons();
+	const project_container = document.querySelector(".project-grid");
+	project_container.innerHTML = "";
 
-	const projectContainer = document.querySelector(".project-grid");
-	const NUM_PROJECTS = 12;
-	const PROJECT_DESC_WORDS = 15;
-	projectContainer.innerHTML = "";
+	for (let i = 1; i <= num_projects; i++) {
+		const project_card = document.createElement("div");
+		project_card.className = "project-card";
 
-	for (let i = 1; i <= NUM_PROJECTS; i++) {
-		const projectCard = document.createElement("div");
-		projectCard.className = "project-card";
+		project_card.innerHTML = `
+    <img src="https://picsum.photos/id/${Math.floor(Math.random() * 1085)}/400/200?grayscale&blur=2" alt="Project ${i} Image">
+    <h3>Project ${i}</h3>
+      <p>${generate_random_text(project_description_words)}</p>
+  `;
 
-		projectCard.innerHTML = `
-      <img src="https://picsum.photos/id/${Math.floor(Math.random() * 1085)}/400/200?grayscale&blur=2" alt="Project ${i} Image">
-      <h3>Project ${i}</h3>
-            <p>${generateRandomText(PROJECT_DESC_WORDS)}</p>
-    `;
-
-		projectContainer.appendChild(projectCard);
+		project_container.appendChild(project_card);
 	}
 });
